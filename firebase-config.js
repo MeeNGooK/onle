1f0269342ffd28e37b2c403d2cceb6690f0ac039
MeeNGooK <rjrj0524@gmail.com>
2026-07-01 18:41:36 +0900

    Enable Firebase config for Firestore sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyC9cg2J-nzqOWgWQ00LuO7R7b9oWEGoJxE",
+  authDomain: "onle-6e4a1.firebaseapp.com",
+  projectId: "onle-6e4a1",
+  storageBucket: "onle-6e4a1.firebasestorage.app",
+  messagingSenderId: "190716573777",
+  appId: "1:190716573777:web:28c04032b7df509a436070",
 };
 
 export const firebaseEnabled = Boolean(
